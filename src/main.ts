3590ea22cb751e4d5bedc0aa000dacc09fae0d28
Irfan Arshad <mirfanarshad320az@gmail.com>
2026-04-28 08:48:25 +0500

    "Port Changes for deploy"

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   
-  const port = process.env.PORT ?? 3001;
+  const port = process.env.PORT ?? 8080;
   await app.listen(port);
   console.log(`
     ✅ Backend running on port ${port}
